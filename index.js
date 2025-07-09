c0d36ffc682da262d9e1b779f90a04be144030da
Nowshin Nawar <68994761+Nowshin73@users.noreply.github.com>
2025-07-09 22:33:45 +0600

    update

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require("body-parser");
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zmagebg.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// Connect the client to the server	(optional starting in v4.7)
+//await client.connect();
+const tasksCollection = client.db("todolist").collection("tasks");
+const userCollection = client.db("todolist").collection("users");
+
+//tasks
+app.get("/tasks", async (req, res) =>{
+    const result = await tasksCollection.find().toArray();
+    res.send(result);
+});
+
+app.post("/tasks", async (req, res) =>{
+    const task = req.body;
+    const result = await tasksCollection.insertOne(task);
+    res.send(result);
+
+})
+
+app.delete("/tasks/:id", async (req, res) =>{
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id)}
+    const result = await tasksCollection.deleteOne(query);
+    res.send(result);
+})
+app.put('/tasks/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const updatedtasks = req.body;
+  const result = await tasksCollection.replaceOne(filter, updatedtasks);
+  res.send(result);
+})
+// Send a ping to confirm a successful connection
+//await client.db("admin").command({ ping: 1 });
+console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+//run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('todoList  is running')
+})
+
+app.listen(port, () => {
+  console.log(`TodoList is running on port ${port}`);
+})
