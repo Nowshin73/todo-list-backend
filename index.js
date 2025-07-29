9cdd8252ca2117c225b27b943cd15bcae245428a
Nowshin Nawar <68994761+Nowshin73@users.noreply.github.com>
2025-07-29 20:58:24 +0600

    put updated

--- index.js
+++ index.js
@@ -30,47 +30,49 @@ const tasksCollection = client.db("todolist").collection("tasks");
 const userCollection = client.db("todolist").collection("users");
 
 //tasks
-app.get("/tasks", async (req, res) =>{
-    const result = await tasksCollection.find().toArray();
-    res.send(result);
+app.get("/tasks", async (req, res) => {
+  const result = await tasksCollection.find().toArray();
+  res.send(result);
 });
 
-app.post("/tasks", async (req, res) =>{
-    const task = req.body;
-    const result = await tasksCollection.insertOne(task);
-    res.send(result);
+app.post("/tasks", async (req, res) => {
+  const task = req.body;
+  const result = await tasksCollection.insertOne(task);
+  res.send(result);
 
 })
 
-app.delete("/tasks/:id", async (req, res) =>{
-    const id = req.params.id;
-    const query = { _id: new ObjectId(id)}
-    const result = await tasksCollection.deleteOne(query);
-    res.send(result);
+app.delete("/tasks/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+  const result = await tasksCollection.deleteOne(query);
+  res.send(result);
 })
 app.put('/tasks/:id', async (req, res) => {
   const id = req.params.id;
   const filter = { _id: new ObjectId(id) };
   const updatedtasks = req.body;
+  // const updateDoc = {
+  //   $set: {
+  //     title: updatedtasks.title,
+  //     description: updatedtasks.description,
+  //     isDone: updatedtasks.isDone
+  //   }
+  // };
+
   const result = await tasksCollection.replaceOne(filter, updatedtasks);
- if(result.modifiedCount > 0){
-    res.send(result);
-  }
-  else {
-    res.status(500).send({ message: 'Error in updating user'});
-  }
 })
 
-app.patch('/tasks/:id', async (req, res) =>{
+app.patch('/tasks/:id', async (req, res) => {
   const id = req.params.id;
-  const task = {_id: new ObjectId(id)};
-  const updateIsDone = {$set: { isDone: req.body.isDone }};
+  const task = { _id: new ObjectId(id) };
+  const updateIsDone = { $set: { isDone: req.body.isDone } };
   const result = await tasksCollection.updateOne(task, updateIsDone);
-  if(result.modifiedCount > 0){
+  if (result.modifiedCount > 0) {
     res.send(result);
   }
   else {
-    res.status(500).send({ message: 'Error in updating user'});
+    res.status(500).send({ message: 'Error in updating user' });
   }
 })
 
